f7e1c29e0ccb6a2a42431a5d78417970adf29f04
Keith <keithyatco@gmail.com>
2020-09-11 17:04:19 +0800

    Implemented list in gallery (working)

--- app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.mobilereservation.CustomAdapter;
+import com.example.mobilereservation.DataModel;
 import com.example.mobilereservation.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
+    ArrayList<DataModel> dataModels;
+    private static CustomAdapter adapter;
     private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,11 +32,34 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final ListView listView = root.findViewById(R.id.list);
+        dataModels= new ArrayList<>();
+
+        dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+        dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+        dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+        dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
+        dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+        dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
+        dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
+        dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
+        dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
+        dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
+        dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
+        dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
+        dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
+
+        adapter= new CustomAdapter(dataModels, getActivity().getApplicationContext() );
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                DataModel dataModel= dataModels.get(position);
+
+                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
             }
         });
         return root;
