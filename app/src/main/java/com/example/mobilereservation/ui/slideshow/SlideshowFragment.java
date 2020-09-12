16ebc3656edbd6af416903f04958217ff9d89fc9
Keith <keithyatco@gmail.com>
2020-09-12 16:43:25 +0800

    Implemented data binding

--- app/src/main/java/com/example/mobilereservation/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/slideshow/SlideshowFragment.java
@@ -4,32 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mobilereservation.R;
+import com.example.mobilereservation.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    FragmentSlideshowBinding mBinding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_slideshow, container, false);
+        mBinding.tvHeading.setText("Welcome to JournalDev.com");
+        mBinding.tvSubHeading.setText("Welcome to this Android Tutorial on DataBinding");
+        View root = mBinding.getRoot();
+
         return root;
     }
 }
