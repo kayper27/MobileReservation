f648ae2721b6e5879ed2ba762774f06fd83b076d
Keith <keithyatco@gmail.com>
2020-09-12 17:37:15 +0800

    Implemented SearchView (working)

---
+++ app/src/main/java/com/example/mobilereservation/ListAdapter.java
@@ -0,0 +1,96 @@
+package com.example.mobilereservation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.example.mobilereservation.databinding.SearchRowItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdapter extends BaseAdapter implements Filterable {
+
+    List<String> mData;
+    List<String> mStringFilterList;
+    ValueFilter valueFilter;
+    private LayoutInflater inflater;
+
+    public ListAdapter(List<String> cancel_type) {
+        mData=cancel_type;
+        mStringFilterList = cancel_type;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, final ViewGroup parent) {
+
+        if (inflater == null) {
+            inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        SearchRowItemBinding rowItemBinding = DataBindingUtil.inflate(inflater, R.layout.search_row_item, parent, false);
+        rowItemBinding.stringName.setText(String.valueOf(mData.get(position)));
+
+        return rowItemBinding.getRoot();
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (valueFilter == null) {
+            valueFilter = new ValueFilter();
+        }
+        return valueFilter;
+    }
+
+    private class ValueFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null && constraint.length() > 0) {
+                List<String> filterList = new ArrayList<>();
+                for (int i = 0; i < mStringFilterList.size(); i++) {
+                    if ((mStringFilterList.get(i).toUpperCase()).contains(constraint.toString().toUpperCase())) {
+                        filterList.add(mStringFilterList.get(i));
+                    }
+                }
+                results.count = filterList.size();
+                results.values = filterList;
+            } else {
+                results.count = mStringFilterList.size();
+                results.values = mStringFilterList;
+            }
+            return results;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint,
+                                      FilterResults results) {
+            mData = (List<String>) results.values;
+            notifyDataSetChanged();
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/mobilereservation/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/slideshow/SlideshowFragment.java
@@ -4,23 +4,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.example.mobilereservation.ListAdapter;
 import com.example.mobilereservation.R;
 import com.example.mobilereservation.databinding.FragmentSlideshowBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SlideshowFragment extends Fragment {
 
+    ListAdapter adapter;
+    List<String> arrayList= new ArrayList<>();
     FragmentSlideshowBinding mBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_slideshow, container, false);
-        mBinding.tvHeading.setText("Welcome to JournalDev.com");
-        mBinding.tvSubHeading.setText("Welcome to this Android Tutorial on DataBinding");
+        arrayList.add("January");
+        arrayList.add("February");
+        arrayList.add("March");
+        arrayList.add("April");
+        arrayList.add("May");
+        arrayList.add("June");
+        arrayList.add("July");
+        arrayList.add("August");
+        arrayList.add("September");
+        arrayList.add("October");
+        arrayList.add("November");
+        arrayList.add("December");
+        adapter = new ListAdapter(arrayList);
+        mBinding.listView.setAdapter(adapter);
+
+        mBinding.search.setActivated(true);
+        mBinding.search.setQueryHint("Type your keyword here");
+        mBinding.search.onActionViewExpanded();
+        mBinding.search.setIconified(false);
+        mBinding.search.clearFocus();
         View root = mBinding.getRoot();
+        mBinding.search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
 
         return root;
     }
