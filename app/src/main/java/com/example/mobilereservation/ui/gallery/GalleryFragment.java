58013dbded9d97d41c1c3d5bbbd667e598f86d5f
Keith <keithyatco@gmail.com>
2020-09-18 02:38:02 +0800

    Clean gallery fragment

--- app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
@@ -4,66 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-import com.example.mobilereservation.DataModel;
 import com.example.mobilereservation.R;
-import com.example.mobilereservation.SearchListAdapter;
 import com.example.mobilereservation.databinding.FragmentGalleryBinding;
-
-import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding maindBinding;
-    private SearchListAdapter adapterSearch;
-
-    //Layout variables
-    ArrayList<DataModel> dataModels = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         maindBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_gallery, container, false);
 
-        dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
-        dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
-        dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
-        dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
-        dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
-        dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
-        dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
-        dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
-        dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
-        dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
-        dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
-        dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
-        dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
-
-        adapterSearch = new SearchListAdapter(getActivity().getApplicationContext(), dataModels);
-        maindBinding.list.setAdapter(adapterSearch);
-
-        maindBinding.searchList.setActivated(true);
-        maindBinding.searchList.setQueryHint("Type your keyword here");
-        maindBinding.searchList.onActionViewExpanded();
-        maindBinding.searchList.setIconified(false);
-        maindBinding.searchList.clearFocus();
-
-        maindBinding.searchList.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                System.out.println("Test Here !");
-                adapterSearch.getFilter().filter(newText);
-                return false;
-            }
-        });
         return maindBinding.getRoot();
     }
 }
