a1c439d2c95653d3089c89bc906ed3cbbace636b
Keith <keithyatco@gmail.com>
2020-09-19 05:08:22 +0800

    Implementing Equipment to display not working

--- app/src/main/java/com/example/mobilereservation/ui/equipment/EquipmentExpandableListAdapter.java
+++ app/src/main/java/com/example/mobilereservation/ui/equipment/EquipmentExpandableListAdapter.java
@@ -1,4 +1,94 @@
 package com.example.mobilereservation.ui.equipment;
 
-public class EquipmentExpandableListAdapter {
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.mobilereservation.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class EquipmentExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private EquipmentListAdapter adapter;
+    private Context context;
+    private List<String> expandableListTitle;
+    private HashMap<String, List<EquipmentModel>> expandableListDetail;
+
+    public EquipmentExpandableListAdapter(Context context, List<String> expandableListTitle, HashMap<String, List<EquipmentModel>> expandableListDetail) {
+        this.context = context;
+        this.expandableListTitle = expandableListTitle;
+        this.expandableListDetail = expandableListDetail;
+    }
+
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition)).get(expandedListPosition);
+    }
+
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+//        adapter = new EquipmentListAdapter( expandableListDetail , expandedListPosition, parent.getContext().getApplicationContext());
+//        ListView expandedListTextView = (ListView) convertView.findViewById(R.id.equipment_list);
+//        expandedListTextView.setAdapter(adapter);
+        return null;
+    }
+
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableListTitle.get(listPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.expandableListTitle.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+        TextView listTitleTextView = (TextView) convertView.findViewById(R.id.listTitle);
+        listTitleTextView.setTypeface(null, Typeface.BOLD);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
+    }
 }
