910c814aae661133bc1d1174c3897c61ec8fbfeb
Keith <keithyatco@gmail.com>
2020-09-18 19:43:58 +0800

    Cleaning GalleryFragment

--- app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/gallery/GalleryFragment.java
@@ -6,19 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobilereservation.R;
-import com.example.mobilereservation.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding maindBinding;
+//    private FragmentGalleryBinding maindBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        maindBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_gallery, container, false);
-
-        return maindBinding.getRoot();
+//        maindBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        return root;
     }
 }
