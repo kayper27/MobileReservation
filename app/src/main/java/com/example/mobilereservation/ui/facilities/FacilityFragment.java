a542c62de1f04e21297d4e7a0cd7200210c8d22f
Keith <keithyatco@gmail.com>
2020-09-22 19:36:38 +0800

    Rename facility fragment to fragment facility

--- app/src/main/java/com/example/mobilereservation/ui/facilities/FacilityFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/facilities/FacilityFragment.java
@@ -12,21 +12,21 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobilereservation.R;
-import com.example.mobilereservation.databinding.FacilityFragmentBinding;
+import com.example.mobilereservation.databinding.FragmentFacilityBinding;
 
 import java.util.ArrayList;
 
 public class FacilityFragment extends Fragment {
 
     private static final String URL = "http://192.168.85.194:3000/";
-    private FacilityFragmentBinding facilityBinding;
+    private FragmentFacilityBinding facilityBinding;
     private FacilitySearchAdapter adapterSearch;
 
     ArrayList<FacilityModel> facilityModel = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        facilityBinding =  DataBindingUtil.inflate(inflater, R.layout.facility_fragment, container, false);
+        facilityBinding =  DataBindingUtil.inflate(inflater, R.layout.fragment_facility, container, false);
 
         facilityModel.add(new FacilityModel("R00","Facility","Available", "This is data is hard coded 0"));
         facilityModel.add(new FacilityModel("R01","Facility","Available", "This is data is hard coded 1"));
