135b106c7dc8d5c53f1b6c72f1adc40e4731bdb1
Keith <keithyatco@gmail.com>
2020-10-01 00:20:21 +0800

    Implemented Request Reservation

---
+++ app/src/main/java/com/example/mobilereservation/ui/reservation/ReservationFragment.java
@@ -0,0 +1,36 @@
+package com.example.mobilereservation.ui.reservation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.mobilereservation.R;
+
+public class ReservationFragment extends Fragment {
+
+    private ReservationViewModel mViewModel;
+
+    public static ReservationFragment newInstance() {
+        return new ReservationFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reservation, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ReservationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/mobilereservation/ui/reservation/ReservationViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mobilereservation.ui.reservation;
+
+import androidx.lifecycle.ViewModel;
+
+public class ReservationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
