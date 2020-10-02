446c354fae41494088f3075d76ed2e18cfc9bc52
Keith <keithyatco@gmail.com>
2020-10-03 02:52:23 +0800

    Working Code for debugging code in equipments

--- app/src/main/java/com/example/mobilereservation/adapters/expandableList/EquipmentExpandableListDataPump.java
+++ app/src/main/java/com/example/mobilereservation/adapters/expandableList/EquipmentExpandableListDataPump.java
@@ -1,18 +1,11 @@
 package com.example.mobilereservation.adapters.expandableList;
 
 import android.content.Context;
-import android.util.Log;
 
-import com.example.mobilereservation.network.ApiClient;
-import com.example.mobilereservation.network.apiService.equipment;
 import com.example.mobilereservation.network.model.Equipment;
 
 import java.util.HashMap;
 import java.util.List;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
 
 public class EquipmentExpandableListDataPump {
 
@@ -25,20 +18,7 @@ public class EquipmentExpandableListDataPump {
     public static HashMap<String, List<Equipment>> getData() {
         final HashMap<String, List<Equipment>> expandableListDetail = new HashMap<String, List<Equipment>>();
 
-        equipment api = ApiClient.getClient(context).create(equipment.class);
-        DisposableSingleObserver<List<Equipment>> error = api.getEquipments()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<List<Equipment>>() {
-                    @Override
-                    public void onSuccess(List<Equipment> equipments) {
 
-                    }
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d(String.valueOf(context), "Error in fetching Equipment "+e.getMessage());
-                    }
-                });
 
         return expandableListDetail;
     }
--- app/src/main/java/com/example/mobilereservation/ui/equipment/EquipmentFragment.java
+++ app/src/main/java/com/example/mobilereservation/ui/equipment/EquipmentFragment.java
@@ -1,6 +1,7 @@
 package com.example.mobilereservation.ui.equipment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,17 @@ import androidx.fragment.app.Fragment;
 
 import com.example.mobilereservation.R;
 import com.example.mobilereservation.adapters.expandableList.EquipmentExpandableListAdapter;
-import com.example.mobilereservation.adapters.expandableList.EquipmentExpandableListDataPump;
+import com.example.mobilereservation.network.ApiClient;
+import com.example.mobilereservation.network.apiService.equipment;
 import com.example.mobilereservation.network.model.Equipment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class EquipmentFragment extends Fragment {
 
@@ -31,10 +37,56 @@ public class EquipmentFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_equipment, container, false);
 
         expandableListView = root.findViewById(R.id.equipmentExpandableListView);
-        expandableListDetail = EquipmentExpandableListDataPump.getData();
-        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
-        expandableListAdapter = new EquipmentExpandableListAdapter(getActivity().getApplicationContext(), getActivity().getSupportFragmentManager(), expandableListTitle, expandableListDetail);
-        expandableListView.setAdapter(expandableListAdapter);
+
+        equipment api = ApiClient.getClient(getActivity().getApplicationContext()).create(equipment.class);
+        DisposableSingleObserver<List<Equipment>> error = api.getEquipments()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<Equipment>>() {
+                    @Override
+                    public void onSuccess(List<Equipment> equipments) {
+                        final HashMap<String, List<Equipment>> expandalbleList = new HashMap<>();
+
+                        ArrayList<List<Equipment>> equipmentSort = new ArrayList<>();
+                        List<Equipment> equips = new ArrayList<>();
+
+                        equips.add(new Equipment("projector0","Equipment", "Available", "EPSON", "x0000", "Projector", "This is projector data hard data 0"));
+                        equips.add(new Equipment("projector1","Equipment", "Available", "EPSON", "x0000", "Projector", "This is projector data hard data 1"));
+                        equips.add(new Equipment("projector2","Equipment", "Available", "EPSON", "x0000", "Projector", "This is projector data hard data 2"));
+                        equips.add(new Equipment("projector3","Equipment", "Available", "EPSON", "x0000", "Projector", "This is projector data hard data 3"));
+                        equips.add(new Equipment("projector4","Equipment", "Available", "EPSON", "x0000", "Projector", "This is projector data hard data 4"));
+                        equipmentSort.add(equips);
+                        equips = new ArrayList<>();
+                        equips.add(new Equipment("computer0","Equipment", "Available", "MSI", "x0000", "Computer", "This is computer data hard data 0"));
+                        equips.add(new Equipment("computer1","Equipment", "Available", "MSI", "x0000", "Computer", "This is computer data hard data 1"));
+                        equips.add(new Equipment("computer2","Equipment", "Available", "MSI", "x0000", "Computer", "This is computer data hard data 2"));
+                        equips.add(new Equipment("computer3","Equipment", "Available", "MSI", "x0000", "Computer", "This is computer data hard data 3"));
+                        equips.add(new Equipment("computer4","Equipment", "Available", "MSI", "x0000", "Computer", "This is computer data hard data 4"));
+                        equipmentSort.add(equips);
+
+                        System.out.println("TEST "+equipmentSort.size());
+                        System.out.println("TEST "+equipmentSort.get(0).size());
+                        System.out.println("TEST "+equipmentSort.get(1).size());
+                        for(int i = 0; i < equipmentSort.size(); i++){
+                            expandalbleList.put(equipmentSort.get(i).get(0).getType().toUpperCase(), equipmentSort.get(i));
+                            for(int x = 0; x < equipmentSort.get(i).size(); x++){
+                                System.out.println("TEST Data |"+ i +"| "+ x +" "+equipmentSort.get(i).get(x).getType());
+                            }
+                        }
+
+
+                        System.out.println("TEST END");
+
+                        expandableListDetail =  expandalbleList;
+                        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+                        expandableListAdapter = new EquipmentExpandableListAdapter(getActivity().getApplicationContext(), getActivity().getSupportFragmentManager(), expandableListTitle, expandableListDetail);
+                        expandableListView.setAdapter(expandableListAdapter);
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(String.valueOf(getActivity().getApplicationContext()), "Error in fetching Equipment "+e.getMessage());
+                    }
+                });
 
         expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
             @Override
