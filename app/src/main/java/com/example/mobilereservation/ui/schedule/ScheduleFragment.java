b93a8df7876d4b1618770887dd311a05cd39ab23
Keith <keithyatco@gmail.com>
2020-10-05 02:21:17 +0800

    Implemented Schedule page

---
+++ app/src/main/java/com/example/mobilereservation/ui/schedule/ScheduleFragment.java
@@ -0,0 +1,35 @@
+package com.example.mobilereservation.ui.schedule;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.mobilereservation.R;
+
+public class ScheduleFragment extends Fragment {
+
+    private ScheduleViewModel mViewModel;
+
+    public static ScheduleFragment newInstance() {
+        return new ScheduleFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ScheduleViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/mobilereservation/ui/schedule/ScheduleViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mobilereservation.ui.schedule;
+
+import androidx.lifecycle.ViewModel;
+
+public class ScheduleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
