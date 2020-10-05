ce4d0811bcc9f6e83be002e95d5bb7114cc0c438
Keith <keithyatco@gmail.com>
2020-10-06 02:30:26 +0800

    Implementing MVVM in Request (not working)

--- app/src/main/java/com/example/mobilereservation/view/reservation/ReservationFragment.java
+++ app/src/main/java/com/example/mobilereservation/view/reservation/ReservationFragment.java
@@ -7,15 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mobilereservation.R;
+import com.example.mobilereservation.databinding.FragmentReservationBinding;
 import com.example.mobilereservation.viewModel.ReservationViewModel;
 
 public class ReservationFragment extends Fragment {
 
-    private ReservationViewModel mViewModel;
+    FragmentReservationBinding fragmentReservationBinding;
 
     public static ReservationFragment newInstance() {
         return new ReservationFragment();
@@ -23,14 +24,11 @@ public class ReservationFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reservation, container, false);
+        fragmentReservationBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_reservation, container, false);
+        fragmentReservationBinding.setViewModel((new ReservationViewModel(getActivity().getApplicationContext())));
+        fragmentReservationBinding.executePendingBindings();
+        return fragmentReservationBinding.getRoot();
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(ReservationViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/com/example/mobilereservation/viewModel/ReservationViewModel.java
+++ app/src/main/java/com/example/mobilereservation/viewModel/ReservationViewModel.java
@@ -1,10 +1,105 @@
 package com.example.mobilereservation.viewModel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
-public class ReservationViewModel extends ViewModel {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.mobilereservation.BR;
+import com.example.mobilereservation.model.Equips;
+import com.example.mobilereservation.model.Request;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class ReservationViewModel extends BaseObservable {
+
+    private Context context;
+    private Request request;
+    private Equips equipment;
+    private List<String> equipment_id;
+    private List<String> equipment_status;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
 
+    public ReservationViewModel(Context context){
+        this.context = context;
 
-    // TODO: Implement the ViewModel
+        equipment.add(equipment_id)
+
+        request = new Request(
+                "",
+                "",
+                "",
+                "",
+                equipment
+        );
+    }
+
+    public void setRequestStartAt(String requestStart) {
+        request.setStartAt(requestStart);
+        notifyPropertyChanged(BR.requestStartAt);
+    }
+
+    public void setRequestEndAt(String requestEnd) {
+        request.setStartAt(requestEnd);
+        notifyPropertyChanged(BR.requestEndAt);
+    }
+
+    @Bindable
+    public String getRequestStartAt() {
+        return request.getStartAt();
+    }
+
+    @Bindable
+    public String getRequestEndAt() {
+        return request.getEndAt();
+    }
+
+
+    public void setDateTime(View v){
+
+        // Get Current Date
+        final Calendar date = Calendar.getInstance();
+        mYear = date.get(Calendar.YEAR);
+        mMonth = date.get(Calendar.MONTH);
+        mDay = date.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+//                        txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+        // Get Current Time
+        final Calendar time = Calendar.getInstance();
+        mHour = time.get(Calendar.HOUR_OF_DAY);
+        mMinute = time.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog;
+        timePickerDialog = new TimePickerDialog(context,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+//                        txtTime.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+
+
+    }
 }
