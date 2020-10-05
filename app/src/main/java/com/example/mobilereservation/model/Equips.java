2a552a17dcacfae14872e0006098f803239c444f
Keith <keithyatco@gmail.com>
2020-10-06 02:29:46 +0800

    Fixing model in Request

---
+++ app/src/main/java/com/example/mobilereservation/model/Equips.java
@@ -0,0 +1,17 @@
+package com.example.mobilereservation.model;
+
+import java.util.List;
+
+public class Equips{
+    private List<String> equipment_id;
+    private List<String> equipment_status;
+
+    public List<String> getEquipment_id() {
+        return equipment_id;
+    }
+
+    public List<String> getEquipment_Status() {
+        return equipment_status;
+    }
+
+}
--- app/src/main/java/com/example/mobilereservation/model/Request.java
+++ app/src/main/java/com/example/mobilereservation/model/Request.java
@@ -22,14 +22,20 @@ public class Request {
         this.endAt = endAt;
         this.facility_id = facility_id;
         this.equipment = equipment;
-        this.equipment_id = equipment.equipment_id;
-        this.equipment_status = equipment.status;
+        this.equipment_id = equipment.getEquipment_id();
+        this.equipment_status = equipment.getEquipment_Status();
     }
 
-    public class Equips{
-        private List<String> equipment_id;
-        private List<String> status;
+    public Request(String username, String startAt, String endAt, String facility_id, Equips equipment){
+        this.username = username;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.facility_id = facility_id;
+        this.equipment = equipment;
+        this.equipment_id = equipment.getEquipment_id();
+        this.equipment_status = equipment.getEquipment_Status();
     }
+
 
     public String getRequest_id() {
         return _id;
@@ -65,6 +71,34 @@ public class Request {
 
     public List<String> getEquipment_status() {
         return equipment_status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setStartAt(String startAt) {
+        this.startAt = startAt;
+    }
+
+    public void setEndAt(String endAt) {
+        this.endAt = endAt;
+    }
+
+    public void setFacility_id(String facility_id) {
+        this.facility_id = facility_id;
+    }
+
+    public void setEquipment(Equips equipment) {
+        this.equipment = equipment;
+    }
+
+    public void setEquipment_id(List<String> equipment_id) {
+        this.equipment_id = equipment_id;
     }
 
     public void setEquipment_status(List<String> equipment_status) {
