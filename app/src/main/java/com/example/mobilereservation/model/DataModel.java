f762b2f4521516cd9b037ddb1abda434b2c07179
Keith <keithyatco@gmail.com>
2020-10-11 02:07:33 +0800

    Working checkbox in To Return (just a test)

---
+++ app/src/main/java/com/example/mobilereservation/model/DataModel.java
@@ -0,0 +1,13 @@
+package com.example.mobilereservation.model;
+
+public class DataModel {
+    public String name;
+    public boolean checked;
+
+
+    public DataModel(String name, boolean checked) {
+        this.name = name;
+        this.checked = checked;
+
+    }
+}
--- app/src/main/java/com/example/mobilereservation/view/toReturn/ToReturnFragment.java
+++ app/src/main/java/com/example/mobilereservation/view/toReturn/ToReturnFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +14,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mobilereservation.R;
+import com.example.mobilereservation.adapters.CustomAdapter;
+import com.example.mobilereservation.model.DataModel;
 import com.example.mobilereservation.viewModel.ToReturnViewModel;
+
+import java.util.ArrayList;
 
 public class ToReturnFragment extends Fragment {
 
     private ToReturnViewModel mViewModel;
+    ArrayList dataModels;
+    ListView listView;
+    private CustomAdapter adapter;
 
     public static ToReturnFragment newInstance() {
         return new ToReturnFragment();
@@ -23,7 +33,40 @@ public class ToReturnFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_to_return, container, false);
+        View root = inflater.inflate(R.layout.fragment_to_return, container, false);
+        listView = (ListView) root.findViewById(R.id.listView);
+
+        dataModels = new ArrayList();
+
+        dataModels.add(new DataModel("Apple Pie", false));
+        dataModels.add(new DataModel("Banana Bread", false));
+        dataModels.add(new DataModel("Cupcake", false));
+        dataModels.add(new DataModel("Donut", true));
+        dataModels.add(new DataModel("Eclair", true));
+        dataModels.add(new DataModel("Froyo", true));
+        dataModels.add(new DataModel("Gingerbread", true));
+        dataModels.add(new DataModel("Honeycomb", false));
+        dataModels.add(new DataModel("Ice Cream Sandwich", false));
+        dataModels.add(new DataModel("Jelly Bean", false));
+        dataModels.add(new DataModel("Kitkat", false));
+        dataModels.add(new DataModel("Lollipop", false));
+        dataModels.add(new DataModel("Marshmallow", false));
+        dataModels.add(new DataModel("Nougat", false));
+
+        adapter = new CustomAdapter(dataModels, getActivity().getApplicationContext());
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+
+                DataModel dataModel= (DataModel) dataModels.get(position);
+                dataModel.checked = !dataModel.checked;
+                Toast.makeText(getContext().getApplicationContext(), "Hello from "+dataModel.name, Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
+            }
+        });
+        return root;
     }
 
     @Override
