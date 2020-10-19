19db9fca8c9e80203033fdedc856a1fc24ee22fa
Keith <keithyatco@gmail.com>
2020-10-19 21:52:56 +0800

    Implemented AsyncTask in Request

--- app/src/main/java/com/example/mobilereservation/view/request/ReqeuestFragment.java
+++ app/src/main/java/com/example/mobilereservation/view/request/ReqeuestFragment.java
@@ -1,9 +1,9 @@
 package com.example.mobilereservation.view.request;
 
-import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +20,7 @@ import com.example.mobilereservation.model.Request;
 import com.example.mobilereservation.network.ApiClient;
 import com.example.mobilereservation.network.apiService.request;
 import com.example.mobilereservation.util.convertUtcToLocal;
+import com.example.mobilereservation.view.dialog.ErrorDialogFragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -48,74 +49,10 @@ public class ReqeuestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_reqeuest, container, false);
 
+        RequestAsyncTask asyncTask = new RequestAsyncTask();
+        asyncTask.execute();
+
         expandableListView = root.findViewById(R.id.requestExpandableListView);
-        request api = ApiClient.getClient(getActivity().getApplicationContext()).create(request.class);
-        DisposableSingleObserver<List<Request>> error = api.getUserRequest("2015105910")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<List<Request>>() {
-                    @RequiresApi(api = Build.VERSION_CODES.O)
-                    @Override
-                    public void onSuccess(List<Request> requests) {
-                        final HashMap<String, List<Request>> expandalbleList = new HashMap<>();
-                        convertUtcToLocal dateTime = new convertUtcToLocal();
-                        if(0 > requests.size()){
-                            return;
-                        }
-
-                        for(int i = 0; i < requests.size(); i++){
-                            reqst.add(new Request(
-                                    requests.get(i).getRequest_id(),
-                                    requests.get(i).getStatus(),
-                                    requests.get(i).getUsername(),
-                                    dateTime.formatDateTime(requests.get(i).getStartAt()),
-                                    dateTime.formatDateTime(requests.get(i).getEndAt()),
-                                    requests.get(i).getFacility(),
-                                    requests.get(i).getEquipment()));
-
-                            if(i == requests.size()-1){
-                                saveSeparatedRequest();
-                            }
-                            else if(!requests.get(i).getStatus().equals(requests.get(i+1).getStatus())) {
-                                saveSeparatedRequest();
-                            }
-                        }
-                        for(int i = 0; i < requestSeparated.size(); i++){
-                            expandalbleList.put(requestSeparated.get(i).get(0).getStatus().toUpperCase(), requestSeparated.get(i));
-                        }
-
-                        HashMap arrangedRequest = new LinkedHashMap();
-                        TreeMap<String, List<Request>> map = new TreeMap<>(expandalbleList);
-                        Set set2 = map.entrySet();
-                        Iterator iterator2 = set2.iterator();
-                        while(iterator2.hasNext()) {
-                            Map.Entry me2 = (Map.Entry)iterator2.next();
-                            arrangedRequest.put(me2.getKey().toString(), (List<Equipment>)me2.getValue());
-                        }
-
-                        expandableListDetail = arrangedRequest;
-                        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
-                        expandableListAdapter = new RequestExpandableListAdapter(getActivity().getApplicationContext(), getActivity().getSupportFragmentManager(), expandableListTitle, expandableListDetail);
-                        expandableListView.setAdapter(expandableListAdapter);
-                        for(int i = 0; i < requestSeparated.size(); i++){
-                            if(requestSeparated.get(i).size() != 0){
-                                expandableListView.expandGroup(i);
-                            }
-                        }
-                    }
-                    @Override
-                    public void onError(Throwable e) {
-                        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
-                        alertDialog.setTitle("Error");
-                        System.out.println("|Test| "+ e);
-                        System.out.println("|Test| "+ e.getMessage());
-                        alertDialog.setMessage(e.getMessage());
-                        alertDialog.show();
-                        Log.d(String.valueOf(getActivity().getApplicationContext()), "Error in fetching Request "+e.getMessage());
-                    }
-                });
-
-
 
         expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
             @Override
@@ -145,4 +82,84 @@ public class ReqeuestFragment extends Fragment {
         requestSeparated.add(reqst);
         reqst = new ArrayList<>();
     }
+
+    private class RequestAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog progressDialog;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            request api = ApiClient.getClient(getActivity().getApplicationContext()).create(request.class);
+            DisposableSingleObserver<List<Request>> error = api.getUserRequest("2015105910")
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(new DisposableSingleObserver<List<Request>>() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        @Override
+                        public void onSuccess(List<Request> requests) {
+                            final HashMap<String, List<Request>> expandalbleList = new HashMap<>();
+                            convertUtcToLocal dateTime = new convertUtcToLocal();
+                            if(0 > requests.size()){
+                                return;
+                            }
+
+                            for(int i = 0; i < requests.size(); i++){
+                                reqst.add(new Request(
+                                        requests.get(i).getRequest_id(),
+                                        requests.get(i).getStatus(),
+                                        requests.get(i).getUsername(),
+                                        dateTime.formatDateTime(requests.get(i).getStartAt()),
+                                        dateTime.formatDateTime(requests.get(i).getEndAt()),
+                                        requests.get(i).getFacility(),
+                                        requests.get(i).getEquipment()));
+
+                                if(i == requests.size()-1){
+                                    saveSeparatedRequest();
+                                }
+                                else if(!requests.get(i).getStatus().equals(requests.get(i+1).getStatus())) {
+                                    saveSeparatedRequest();
+                                }
+                            }
+                            for(int i = 0; i < requestSeparated.size(); i++){
+                                expandalbleList.put(requestSeparated.get(i).get(0).getStatus().toUpperCase(), requestSeparated.get(i));
+                            }
+
+                            HashMap arrangedRequest = new LinkedHashMap();
+                            TreeMap<String, List<Request>> map = new TreeMap<>(expandalbleList);
+                            Set set2 = map.entrySet();
+                            Iterator iterator2 = set2.iterator();
+                            while(iterator2.hasNext()) {
+                                Map.Entry me2 = (Map.Entry)iterator2.next();
+                                arrangedRequest.put(me2.getKey().toString(), (List<Equipment>)me2.getValue());
+                            }
+
+                            expandableListDetail = arrangedRequest;
+                            expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+                            expandableListAdapter = new RequestExpandableListAdapter(getActivity().getApplicationContext(), getActivity().getSupportFragmentManager(), expandableListTitle, expandableListDetail);
+                            expandableListView.setAdapter(expandableListAdapter);
+                            for(int i = 0; i < requestSeparated.size(); i++){
+                                if(requestSeparated.get(i).size() != 0){
+                                    expandableListView.expandGroup(i);
+                                }
+                            }
+                        }
+                        @Override
+                        public void onError(Throwable e) {
+                            ErrorDialogFragment errorDialogFragment = ErrorDialogFragment.newInstance("Error", e.getMessage());
+                            errorDialogFragment.show(getFragmentManager(), "dialog_error");
+                        }
+                    });
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void v){
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(getContext(), "Processing", "Fetching for your requests");
+        }
+    }
+
 }
