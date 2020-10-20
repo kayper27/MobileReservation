edd4ed97a9cf141cc886706953079c6bd4ed6b81
Keith <keithyatco@gmail.com>
2020-10-20 17:56:17 +0800

    Removing code of SlidingUpPanelLayout

--- app/src/main/java/com/example/mobilereservation/view/reservation/ReservationFragment.java
+++ app/src/main/java/com/example/mobilereservation/view/reservation/ReservationFragment.java
@@ -13,7 +13,6 @@ import androidx.fragment.app.Fragment;
 import com.example.mobilereservation.R;
 import com.example.mobilereservation.util.DatePickerFragment;
 import com.example.mobilereservation.util.TimePickerFragment;
-import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +23,6 @@ public class ReservationFragment extends Fragment {
 
     private EditText textStartAt, textEndAt, textFacility;
     private Button buttonAddEquipment;
-    private SlidingUpPanelLayout slidingUpPanelLayout;
 
     public static final int REQUEST_CODE = 11; // Used to identify the result
 
@@ -51,10 +49,6 @@ public class ReservationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_reservation, container, false);
 
-        slidingUpPanelLayout = (SlidingUpPanelLayout) root.findViewById(R.id.sliding_layout);
-        slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
-
-        slidingUpPanelLayout = (SlidingUpPanelLayout) root.findViewById(R.id.sliding_layout);
         textStartAt = (EditText) root.findViewById(R.id.editTextStratAt);
         textEndAt = (EditText) root.findViewById(R.id.editTextEndAt);
         textFacility = (EditText) root.findViewById(R.id.editTextFacility);
@@ -78,17 +72,14 @@ public class ReservationFragment extends Fragment {
         textFacility.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                SlideupFragment slideupFragment = SlideupFragment.newInstance("facility", textStartAt.getText().toString(), textEndAt.getText().toString());
-//                getFragmentManager().beginTransaction().replace(R.id.slidup_fragment, slideupFragment).commit();
-                slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+                BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+                bottomSheetFragment.show(getActivity().getSupportFragmentManager(),"TAG");
             }
         });
         buttonAddEquipment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                SlideupFragment slideupFragment = SlideupFragment.newInstance("equipment", textStartAt.getText().toString(), textEndAt.getText().toString());
-//                getFragmentManager().beginTransaction().replace(R.id.slidup_fragment, slideupFragment).commit();
-                slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+
             }
         });
 
