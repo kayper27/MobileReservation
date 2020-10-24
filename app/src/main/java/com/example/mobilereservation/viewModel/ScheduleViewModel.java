c03b0b660d67ba18746a4c4a753d7e0d9d1528c8
Keith <keithyatco@gmail.com>
2020-10-24 19:20:53 +0800

    Removing ViewModel classes

--- app/src/main/java/com/example/mobilereservation/viewModel/ScheduleViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.mobilereservation.viewModel;
-
-import androidx.databinding.BaseObservable;
-
-public class ScheduleViewModel extends BaseObservable {
-}
--- app/src/main/java/com/example/mobilereservation/viewModel/ToReturnViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.mobilereservation.viewModel;
-
-import androidx.lifecycle.ViewModel;
-
-public class ToReturnViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
