ad65e954b0c4cafe9348991a1770fa1d1db0576c
Keith <keithyatco@gmail.com>
2020-10-24 18:50:30 +0800

    Implemented getSpecificStatus in request api

--- app/src/main/java/com/example/mobilereservation/network/apiService/request.java
+++ app/src/main/java/com/example/mobilereservation/network/apiService/request.java
@@ -9,10 +9,14 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface request {
+
     // Fetch all User Request
     @GET("booking_service/user/{username}")
     Single<List<Request>> getUserRequest(@Path("username") String username);
 
     @GET("booking_service/{start}/{end}")
     Single<List<Request>> getReservedchedule(@Path("start") String start, @Path("end") String end);
+
+    @GET("booking_service/stat/{status}")
+    Single<List<Request>> getSpecificStatus(@Path("status") String status);
 }
