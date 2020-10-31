94eba47440736d36ffbc1d037273bb77c8de7462
Keith <keithyatco@gmail.com>
2020-10-31 20:13:06 +0800

    Implemented Validation in TimePickerFragment

--- app/src/main/java/com/example/mobilereservation/util/TimePickerFragment.java
+++ app/src/main/java/com/example/mobilereservation/util/TimePickerFragment.java
@@ -25,10 +25,11 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     private EditText dateTime;
     private String startData;
 
-    public TimePickerFragment(EditText dateTime, String startData){
+    public TimePickerFragment(EditText dateTime, String startData) {
         this.dateTime = dateTime;
         this.startData = startData;
     }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -39,26 +40,41 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         // Return a new instance of TimePickerDialog
         return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
-
-
+    
     @Override
     public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-        Calendar validateTime = Calendar.getInstance();
+        boolean flag = true;
+
         c.set(Calendar.HOUR_OF_DAY, hour);
         c.set(Calendar.MINUTE, minute);
+        String selectedTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH).format(c.getTime());
+        Log.d(TAG, "onDateSet: " + selectedTime);
 
-        if( 7 <= append(hour, minute) && append(hour, minute) <= 20.30){
-            String selectedTime = new SimpleDateFormat("HH:mm", Locale.ENGLISH).format(c.getTime());
-            Log.d(TAG, "onDateSet: " + selectedTime);
-            dateTime.setText(dateTime.getText()+" "+selectedTime);
+        if(startData.length() != 16){ // START TIME SELECTION
+            if(7 >= append(hour, minute) || append(hour, minute) >= 20.30) { // IF SELECTED VALUE IS GREATER THAN EQUAL 7:00 AM OR SELECTED VALUE GREATER THAN EQUAL 8:30 PM
+                flag = false;
+                ErrorDialogFragment errorDialogFragment = ErrorDialogFragment.newInstance("Invalid input", "Time allowed is only between 7:00 AM - 8:30 PM");
+                errorDialogFragment.show(getActivity().getSupportFragmentManager(), "dialog_error");
+            }
         }
-        else{
-            ErrorDialogFragment errorDialogFragment = ErrorDialogFragment.newInstance("Invalid input", "Time allowed is only from 7:00 AM - 8:30 PM");
-            errorDialogFragment.show(getActivity().getSupportFragmentManager(), "dialog_error");
+        else { // END TIME SELECTION
+            int convertHour = 0, convertMinute = 0;
+            startData = startData.substring(11, 16);
+            convertHour = Integer.parseInt(startData.substring(0, 2));
+            convertMinute = Integer.parseInt(startData.substring(3, 5));
+            if(append(convertHour, convertMinute) >= append(hour, minute)) {  // IF STARTDATA IS GREATER THAN EQUAL TO SELECTED DATA
+                flag = false;
+                ErrorDialogFragment errorDialogFragment = ErrorDialogFragment.newInstance("Invalid input", "Time allowed is only between "+startData+" - 8:30 PM");
+                errorDialogFragment.show(getActivity().getSupportFragmentManager(), "dialog_error");
+            }
+        }
+
+        if(flag){// ONLY ALLOW DATA IF TIME IS VALID
+            dateTime.setText(dateTime.getText() + " " + selectedTime);
         }
     }
 
     public double append(int hour, int minute) {
-        return Double.parseDouble(new StringBuilder().append(hour+".").append(minute).toString());
+        return Double.parseDouble(new StringBuilder().append(hour + ".").append(minute).toString());
     }
 }
