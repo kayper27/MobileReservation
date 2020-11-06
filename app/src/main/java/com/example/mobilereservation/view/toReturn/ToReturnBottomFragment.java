6dca9bfe8da7007fd4b456996ee8c2b5b85d0352
Keith <keithyatco@gmail.com>
2020-11-06 20:59:16 +0800

    Implemented behavior in bottom sheet in ToReturnBottomFragment

--- app/src/main/java/com/example/mobilereservation/view/toReturn/ToReturnBottomFragment.java
+++ app/src/main/java/com/example/mobilereservation/view/toReturn/ToReturnBottomFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
 
 import androidx.databinding.DataBindingUtil;
 
@@ -11,6 +13,8 @@ import com.example.mobilereservation.R;
 import com.example.mobilereservation.adapters.listAdapter.ToReturnEquipmentListAdapter;
 import com.example.mobilereservation.databinding.FragmentToReturnBottomBinding;
 import com.example.mobilereservation.model.Request;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.util.ArrayList;
@@ -21,6 +25,10 @@ public class ToReturnBottomFragment extends BottomSheetDialogFragment {
     private Request request;
 
     private FragmentToReturnBottomBinding fragmentToReturnBottomBinding;
+
+    private FrameLayout bottomSheet;
+    private BottomSheetDialog dialog;
+    private BottomSheetBehavior behavior;
 
     public ToReturnBottomFragment() {
         // Required empty public constructor
@@ -47,9 +55,26 @@ public class ToReturnBottomFragment extends BottomSheetDialogFragment {
         fragmentToReturnBottomBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_to_return_bottom, container, false);
         View root = fragmentToReturnBottomBinding.getRoot();
 
+        root.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                dialog = (BottomSheetDialog) ToReturnBottomFragment.this.getDialog();
+                if (dialog != null) {
+                    bottomSheet = dialog.findViewById(R.id.design_bottom_sheet);
+                    if (bottomSheet != null) {
+                        behavior = BottomSheetBehavior.from(bottomSheet);
+                        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        behavior.setSkipCollapsed(true);
+                        behavior.setHideable(false);
+                    }
+                }
+            }
+        });
+
+
         fragmentToReturnBottomBinding.toReturnRequestId.setText(request.getRequest_id());
 
-        ToReturnEquipmentListAdapter toReturnEquipmentListAdapter = new ToReturnEquipmentListAdapter((ArrayList<String>) request.getEquipment_id(), getActivity().getApplicationContext(), getActivity().getSupportFragmentManager());
+        ToReturnEquipmentListAdapter toReturnEquipmentListAdapter = new ToReturnEquipmentListAdapter((ArrayList<String>) request.getEquipment_id(), getActivity().getApplicationContext());
         fragmentToReturnBottomBinding.toReturnList.setAdapter(toReturnEquipmentListAdapter);
         // Inflate the layout for this fragment
         return root;
