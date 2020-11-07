76db944288f1c0652ef5b3b160b3a309fcab6020
Keith <keithyatco@gmail.com>
2020-11-07 22:34:02 +0800

    Implementing CreateRequest Model

---
+++ app/src/main/java/com/example/mobilereservation/model/CreateRequest.java
@@ -0,0 +1,21 @@
+package com.example.mobilereservation.model;
+
+public class CreateRequest {
+
+    private String id;
+    private String username;
+    private String startAt;
+    private String endAt;
+    private String facility_id;
+    private Equips equipment;
+
+    public CreateRequest(String id, String username, String startAt, String endAt, String facility_id, Equips equipment){
+        this.id = id;
+        this.username = username;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.facility_id = facility_id;
+        this.equipment = new  Equips(equipment.getEquipment_id(), equipment.getEquipment_Status());
+    }
+
+}
