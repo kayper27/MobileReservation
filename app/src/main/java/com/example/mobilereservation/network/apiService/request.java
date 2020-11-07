c442c4437b5f1403545662cfb93496fdb29c1c16
Keith <keithyatco@gmail.com>
2020-11-07 22:34:51 +0800

    Implementing Endpoints for API

--- app/src/main/java/com/example/mobilereservation/network/apiService/request.java
+++ app/src/main/java/com/example/mobilereservation/network/apiService/request.java
@@ -1,11 +1,19 @@
 package com.example.mobilereservation.network.apiService;
 
+import com.example.mobilereservation.model.CreateRequest;
 import com.example.mobilereservation.model.Request;
 
 import java.util.List;
 
 import io.reactivex.Single;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface request {
@@ -19,4 +27,16 @@ public interface request {
 
     @GET("booking_service/stat/{status}")
     Single<List<Request>> getSpecificStatus(@Path("status") String status);
+
+    @POST("booking_service/")
+    @FormUrlEncoded
+    Call<ResponseBody> requestCreate(@Body CreateRequest request);
+
+    @PATCH("booking_service/{request_id}")
+    @FormUrlEncoded
+    Call<ResponseBody> requestUpdate(@Path("request_id") String request_id);
+
+    @DELETE("booking_service/{request_id}")
+    @FormUrlEncoded
+    Call<ResponseBody> requestDelete(@Path("request_id") String username);
 }
