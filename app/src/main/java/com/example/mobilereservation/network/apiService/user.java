3290304db75f035c48ddfba80450ada18b0815e3
Keith <keithyatco@gmail.com>
2020-11-15 00:43:19 +0800

    Fix error of field key account_id in apiService

--- app/src/main/java/com/example/mobilereservation/network/apiService/user.java
+++ app/src/main/java/com/example/mobilereservation/network/apiService/user.java
@@ -6,14 +6,16 @@ import com.example.mobilereservation.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface user {
 
     @Headers({"Access-Control-Allow-Methods: POST"})
-    @POST("users/Login")
-    Call<LoggedInUser> userLogin(@Field("username") String user, @Field("password") String password);
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<LoggedInUser> userLogin(@Field ("account_id") String user, @Field("password") String password);
 
     @Headers({"Access-Control-Allow-Methods: POST"})
     @POST("users/signup")
