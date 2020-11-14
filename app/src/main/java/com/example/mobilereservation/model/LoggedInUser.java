a5effa9f4c0c21af9188a150fecd419f156d69ee
Keith <keithyatco@gmail.com>
2020-11-14 18:30:25 +0800

    Implemented Token Login User

--- app/src/main/java/com/example/mobilereservation/model/LoggedInUser.java
+++ app/src/main/java/com/example/mobilereservation/model/LoggedInUser.java
@@ -5,10 +5,13 @@ public class LoggedInUser {
     private String account_id;
     private String account_type;
     private String status;
+    private String token;
 
-    public LoggedInUser(String account_id, String account_type) {
+    public LoggedInUser(String account_id, String account_type, String status, String token) {
         this.account_id = account_id;
         this.account_type = account_type;
+        this.status = status;
+        this.token = token;
     }
 
     public String getAccount_id() {
@@ -23,7 +26,7 @@ public class LoggedInUser {
         return status;
     }
 
-    public void setUserStatus(String status) {
-        this.status = status;
+    public String getToken() {
+        return token;
     }
 }
