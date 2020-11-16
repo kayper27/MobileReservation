4f46e12fcec4e246c98afbbe1aebe04c1e8d80a7
Keith <keithyatco@gmail.com>
2020-11-17 00:43:27 +0800

    Serializing User model for registration

--- app/src/main/java/com/example/mobilereservation/model/User.java
+++ app/src/main/java/com/example/mobilereservation/model/User.java
@@ -1,6 +1,8 @@
 package com.example.mobilereservation.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     private String account_id;
     private String password;
