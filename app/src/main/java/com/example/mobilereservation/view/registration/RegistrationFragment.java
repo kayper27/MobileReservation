c90572394d27af66d98bcbc2dce265a02783cc83
Keith <keithyatco@gmail.com>
2020-11-17 00:43:00 +0800

    Initial commit for registration

---
+++ app/src/main/java/com/example/mobilereservation/view/registration/RegistrationFragment.java
@@ -0,0 +1,42 @@
+package com.example.mobilereservation.view.registration;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mobilereservation.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RegistrationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegistrationFragment extends Fragment {
+
+    public RegistrationFragment() {
+        // Required empty public constructor
+    }
+
+    public static RegistrationFragment newInstance() {
+        RegistrationFragment fragment = new RegistrationFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        return root;
+    }
+}
