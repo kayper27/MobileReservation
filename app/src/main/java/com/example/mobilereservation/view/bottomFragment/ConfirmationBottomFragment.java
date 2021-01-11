3c609d3a231bf92d7aa469517a4b52d60b85276e
Keith <keithyatco@gmail.com>
2021-01-12 00:00:37 +0800

    Initial Commit of ConfirmationBottomFragment

---
+++ app/src/main/java/com/example/mobilereservation/view/bottomFragment/ConfirmationBottomFragment.java
@@ -0,0 +1,38 @@
+package com.example.mobilereservation.view.bottomFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mobilereservation.R;
+
+public class ConfirmationBottomFragment extends Fragment {
+
+
+    public ConfirmationBottomFragment() {
+        // Required empty public constructor
+    }
+    
+    public static ConfirmationBottomFragment newInstance() {
+        ConfirmationBottomFragment fragment = new ConfirmationBottomFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation_bottom, container, false);
+    }
+}
