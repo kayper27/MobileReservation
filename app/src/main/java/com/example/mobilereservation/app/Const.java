351d256248f063a36f426ec6e4e4196e23a5983f
Keith <keithyatco@gmail.com>
2021-01-11 19:52:33 +0800

    Change target IP

--- app/src/main/java/com/example/mobilereservation/app/Const.java
+++ app/src/main/java/com/example/mobilereservation/app/Const.java
@@ -1,5 +1,5 @@
 package com.example.mobilereservation.app;
 
 public class Const {
-    public static final String BASE_URL = "http://192.168.85.194:3000/api/";
+    public static final String BASE_URL = "http://192.168.156.194:3000/api/";
 }
